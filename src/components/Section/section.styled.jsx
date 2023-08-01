dd98ee2477513a013683362d1392d55ca7e3b906
MariaBetancur <mabo6241@gmail.com>
2023-07-31 21:06:56 -0500

    v1.2

--- src/components/Section/section.styled.jsx
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Sections = styled.section`
-  padding-top: 20px;
-  padding-bottom: 20px;
-`;
--- src/components/Statistics/statistics.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const StatisticsList = styled.ul`
-list-style: none;
-display:flex;
-justify-content: space-between;
-padding-left: 0;
-}
-`;
